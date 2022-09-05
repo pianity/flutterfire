329a37bc2be6047792a4d1efb044797ea8e34f0f
Kiruel <etiennetheodore@orange.fr>
2022-09-05 14:14:39 +0200

    fix: remove only FCM payload

--- packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
+++ packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
@@ -339,12 +339,12 @@ NSString *const kMessagingPresentationOptionsUserDefaults =
   _notificationOpenedAppID = remoteNotification[@"gcm.message_id"];
   // We only want to handle FCM notifications and stop firing `onMessageOpenedApp()` when app is
   // coming from a terminated state.
-  if (_notificationOpenedAppID != nil &&
-      ![_initialNoticationID isEqualToString:_notificationOpenedAppID]) {
+  // if (_notificationOpenedAppID != nil &&
+  //     ![_initialNoticationID isEqualToString:_notificationOpenedAppID]) {
     NSDictionary *notificationDict =
         [FLTFirebaseMessagingPlugin remoteMessageUserInfoToDict:remoteNotification];
     [_channel invokeMethod:@"Messaging#onMessageOpenedApp" arguments:notificationDict];
-  }
+  // }
 
   // Forward on to any other delegates.
   if (_originalNotificationCenterDelegate != nil &&
